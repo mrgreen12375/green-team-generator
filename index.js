4eb3567e127a018653c13de54752df21672a8b0c
Steven Green <ste2144105@gmail.com>
2022-10-12 00:48:27 -0700

    imported classes

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
+const Manager = require('./lib/manager');
+const Engineer = require('./lib/engineer');
+const Intern = require('./lib/intern');
+
 const inquirer = require('inquirer');
 const fs=require('fs');
